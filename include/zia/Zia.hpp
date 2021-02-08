0475269b13b46d216efcde2b667bd19b36195971
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-02-08 12:12:44 +0100

    Set write buf to const char* instead of char* on IOutput

--- include/zia/Zia.hpp
+++ include/zia/Zia.hpp
@@ -80,7 +80,7 @@ public:
 	* @fn write
 	* Write bytes on the stream.
 	* @param size_t buf_size: available bytes count at buf
-	* @param char *buf: buffer with bytes that will be written on the stream
+	* @param const char *buf: buffer with bytes that will be written on the stream
 	* @return size_t: the amount of bytes actually written on the stream
 	* @note This function can very well return 0 if no bytes are available on stream for writing,
 	* even if buf_size is superior to 0. It is reasonnable to keep trying to write those bytes
@@ -88,7 +88,7 @@ public:
 	* in the near future. Also, assume that this availability might never actually come so you should
 	* also take that into consideration.
 	*/
-	virtual size_t write(size_t buf_size, char *buf) = 0;	
+	virtual size_t write(size_t buf_size, const char *buf) = 0;
 };
 
 /**
